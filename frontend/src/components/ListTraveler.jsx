851c0a3e54fe493831d85af70f04a70f29ae986f
farid <faridhimeur@outlook.fr>
2023-01-12 10:22:16 +0100

    finished page list traveler for driver

--- frontend/src/components/ListTraveler.jsx
+++ frontend/src/components/ListTraveler.jsx
@@ -1,18 +1,16 @@
-import arrow from "../assets/arrow.png";
 import "./ListTraveler.css";
 
 export default function ListTraveler() {
   return (
     <ul className="list-of-traveler">
       <li className="traveler">
-        <p className="name-traveler">Prénom Nom</p>
-        <button className="button-start-end-traveler" type="button">
-          <img className="arrow-start-end-traveler" src={arrow} alt="arrow" />
-        </button>
-        <ul className="list-start-end">
-          <li className="list-start">Départ :</li>
-          <li className="list-end">Arrivé : </li>
-        </ul>
+        <details>
+          <summary className="name-traveler">Prénom Nom</summary>
+          <ul className="list-start-end">
+            <li className="list-start">Départ :</li>
+            <li className="list-end">Arrivé : </li>
+          </ul>
+        </details>
       </li>
     </ul>
   );
