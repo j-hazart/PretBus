cdffb6ef6036ac9d91c3f9481478c93d3c2fd826
farid <faridhimeur@outlook.fr>
2023-01-12 11:40:01 +0100

    add path for ListTraveler Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import CreateAccount from "@pages/CreateAccount";
 import Login from "@pages/Login";
+import ListTraveler from "@components/ListTraveler";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -15,6 +16,7 @@ root.render(
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/createaccount" element={<CreateAccount />} />
+          <Route path="/travelerlist" element={<ListTraveler />} />
         </Routes>
       </Router>
     </UserProvider>
